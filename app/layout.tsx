1693c2048ff8f5639d711df8630d7f2abc70fb97
securetykeyless-doto <securetykeyless@gmail.com>
2026-05-11 02:58:21 +0300

    ss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThirdwebProvider } from "thirdweb/react";
+import { Providers } from "./providers"; // Імпортуємо наш новий файл
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* Огортаємо весь сайт у ThirdwebProvider */}
-        <ThirdwebProvider>
+        <Providers>
           {children}
-        </ThirdwebProvider>
+        </Providers>
       </body>
     </html>
   );
